92196d94334ad2f2ae4c9d48beea1fefc8052e48
Owner <deema@danon.tzafonet.org.il>
2021-11-25 09:44:24 +0200

    3333

--- app/src/main/java/neveen/samih/samihtasksmanager/RegisterActivity.java
+++ app/src/main/java/neveen/samih/samihtasksmanager/RegisterActivity.java
@@ -3,6 +3,7 @@ package neveen.samih.samihtasksmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -15,5 +16,29 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        etRePassword=findViewById(R.id.etRePassword);
+        etFullname=findViewById(R.id.etName);
+        btnSave=findViewById(R.id.btnSave);
+
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate();
+            }
+        });
+    }
+
+    private void   validate() {
+        boolean isOk= true;
+        String email = etEmail.getText().toString();
+        String pass1 = etPassword.getText().toString();
+        String pass2 = etRePassword.getText().toString();
+        String name = etFullname.getText().toString();
+
+
+
     }
 }
