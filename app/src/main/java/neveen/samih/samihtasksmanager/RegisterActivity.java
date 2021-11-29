41dbde2663926397a01f876b4255db4eaeb3a645
Owner <deema@danon.tzafonet.org.il>
2021-11-29 08:55:11 +0200

    3333

--- app/src/main/java/neveen/samih/samihtasksmanager/RegisterActivity.java
+++ app/src/main/java/neveen/samih/samihtasksmanager/RegisterActivity.java
@@ -10,7 +10,7 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private TextInputEditText etEmail,etPassword, etRePassword,etFullname;
+    private TextInputEditText etEmail,etPassword, etRePassword,etFullname,etPhone;
     private Button btnSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +21,7 @@ public class RegisterActivity extends AppCompatActivity {
         etRePassword=findViewById(R.id.etRePassword);
         etFullname=findViewById(R.id.etName);
         btnSave=findViewById(R.id.btnSave);
+        etPhone=findViewById(R.id.etPhone);
 
 
         btnSave.setOnClickListener(new View.OnClickListener() {
@@ -37,8 +38,48 @@ public class RegisterActivity extends AppCompatActivity {
         String pass1 = etPassword.getText().toString();
         String pass2 = etRePassword.getText().toString();
         String name = etFullname.getText().toString();
+        String phone =etPhone.getText().toString();
 
+       if(email.length()<5 || email.indexOf('@')<=0)
+       {
+           etEmail.setError("wrong email syntax");
+           isOk=false;
+       }
+       if(phone.length()<10 || phone.length()>10)
+       {
+           etPhone.setError("wrong phone number");
+           isOk=false;
+       }
 
+       if(pass1.length()<8)
+       {
+           etPassword.setError("at least 8 chars");
+           isOk=false;
+       }
+
+       if(pass1.equals(pass2)==false)
+       {
+           etRePassword.setError("not equal password");
+           isOk=false;
+       }
+       if(name.length()==0)
+       {
+           etFullname.setError("must to enter full name");
+           isOk=false;
+       }
+
+       if(isOk)// isok=true
+       {
+
+           createAccount(email,pass1);
+       }
 
     }
+
+    private void createAccount(String email, String pass1)
+    {
+
+    }
+
+
 }
