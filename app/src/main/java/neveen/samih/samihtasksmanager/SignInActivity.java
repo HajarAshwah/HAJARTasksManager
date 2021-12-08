1213cb0202ce1abfa79382c40813272fa9e9f5d8
Owner <deema@danon.tzafonet.org.il>
2021-12-08 08:55:22 +0200

    3333

--- app/src/main/java/neveen/samih/samihtasksmanager/SignInActivity.java
+++ app/src/main/java/neveen/samih/samihtasksmanager/SignInActivity.java
@@ -2,7 +2,9 @@ package neveen.samih.samihtasksmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -18,12 +20,48 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         //2.
-        etEmail=findViewById(R.id.etEmail);
-        etPassword=findViewById(R.id.etPassword);
-        btnSignIn=findViewById(R.id.btnSignIn);
-        btnRegister=findViewById(R.id.btnRegister);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnRegister = findViewById(R.id.btnRegister);
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate();
+            }
 
+        });
+    }
+
+            private void validate()
+            {
+                boolean isOk=true;
+                String email=etEmail.getText().toString();
+                String password=etPassword.getText().toString();
+                if(email.length()==)
+                {
+                    etEmail.setError("enter email");
+                }
+                if (password.length()<8)
+                {
+                    etPassword.setError("password at least 8 letters");
+                }
+                if (isOk)
+                {
+                    signiningIn(email,password);
+                }
+
+    }
+
+    private void signiningIn(String email, String password)
+    {
 
     }
 }
